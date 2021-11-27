d07b7dd3165e071b32606f943d52afd44adf9f8c
Jerzy Radosław Adamski <jadamski@student.wszib.edu.pl>
2021-11-27 14:31:21 +0100

    Initial commit

---
+++ src/SimpleCalc.java
@@ -0,0 +1,8 @@
+public class SimpleCalc {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 1;
+
+        System.out.println(a + " plus " + b + " daje wynik " + (a + b));
+    }
+}
